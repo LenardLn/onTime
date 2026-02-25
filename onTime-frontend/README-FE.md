7c99ee8f8fad807820fa1a30338fdcc360dfa52a
LenardLn <nagylenard2002@gmail.com>
2026-02-25 20:04:57 +0200

    Installed Shadcn/ui dependencies and added Frontend ReadMe

---
+++ onTime-frontend/README-FE.md
@@ -0,0 +1,19 @@
+To start he project successfully:
+
+-- Important! steps marked with `\*` should be done one time only or if dependencies change
+
+1. Navigate to root backend folder
+   cd onTime-frontend
+
+2.\* Install dependencies if necessary
+npm i
+
+3. To start the project
+   npm run dev
+
+4. Install shadcn/ui components
+   npx shadcn@latest add `component_name`
+
+Component should appear in the `components/shadcn` folder
+
+Happy codin`👋
--- onTime-frontend/src/components/map/MapComponent.tsx
+++ onTime-frontend/src/components/map/MapComponent.tsx
@@ -1,20 +1,59 @@
-import { Map, Marker, NavigationControl } from "@vis.gl/react-maplibre";
+import {
+  Map,
+  Marker,
+  NavigationControl,
+  Source,
+  Layer,
+  type MapLayerMouseEvent,
+} from "@vis.gl/react-maplibre";
+import type { FeatureCollection, LineString } from "geojson";
 import "maplibre-gl/dist/maplibre-gl.css";
 import type { Theme } from "../../entities/theme";
+import { useState } from "react";
 
 interface MapComponentProps {
   theme: Theme;
 }
+
+type MapMarker = {
+  latitude: number;
+  longitude: number;
+};
 
 const MapComponent = ({ theme }: MapComponentProps) => {
   const bounds: [[number, number], [number, number]] = [
     [23.439274, 47.617155], // SW [lng, lat]
     [23.729459, 47.686301], // NE [lng, lat]
   ];
+
+  const [markers, setMarkers] = useState<MapMarker[]>([]);
+
+  const addMarker = (e: MapLayerMouseEvent) => {
+    const { lat, lng } = e.lngLat;
+    const newMarker: MapMarker = { latitude: lat, longitude: lng };
+    setMarkers((prev) => [...prev, newMarker]);
+  };
+
+  const routeGeoJSON: FeatureCollection<LineString> = {
+    type: "FeatureCollection",
+    features:
+      markers.length > 1
+        ? [
+            {
+              type: "Feature",
+              geometry: {
+                type: "LineString",
+                coordinates: markers.map((m) => [m.longitude, m.latitude]),
+              },
+              properties: {},
+            },
+          ]
+        : [],
+  };
+
   return (
     <div className="w-screen h-screen rounded-xl">
       <Map
-        // mapStyle="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json" // colorful light
         mapStyle={
           theme === "light"
             ? "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
@@ -28,14 +67,33 @@ const MapComponent = ({ theme }: MapComponentProps) => {
         minZoom={12}
         maxZoom={18}
         maxBounds={bounds}
+        onClick={addMarker}
       >
         <NavigationControl position="top-right" />
-        <Marker
-          longitude={47.662278}
-          latitude={23.579052}
-          color="red"
-          className="z-10"
-        />
+
+        {markers.map((marker) => (
+          <Marker
+            key={marker.latitude}
+            longitude={marker.longitude}
+            latitude={marker.latitude}
+            color="red"
+            className="z-10"
+            onClick={(e) => console.log(e)}
+          />
+        ))}
+
+        {markers.length > 1 && (
+          <Source id="route" type="geojson" data={routeGeoJSON}>
+            <Layer
+              id="route-line"
+              type="line"
+              paint={{
+                "line-color": "#007bff",
+                "line-width": 4,
+              }}
+            />
+          </Source>
+        )}
       </Map>
     </div>
   );
