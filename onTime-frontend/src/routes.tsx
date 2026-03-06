b95e78626aa51d73b3a8b16d9b6bb20c1b61f452
Lucian Stoian <lucians@192.168.68.50>
2026-03-06 13:21:09 +0200

    create register page and validation

--- onTime-frontend/src/routes.tsx
+++ onTime-frontend/src/routes.tsx
@@ -2,13 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/layout";
 import ErrorPage from "./pages/errorPage";
 import HomePage from "./pages/homePage";
+import RegisterPage from "./pages/registerPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     errorElement: <ErrorPage />,
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/register", element: <RegisterPage /> },
+    ],
   },
 ]);
 
